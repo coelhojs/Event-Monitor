e174877b2e3008085ad485e93e7bb2a285bdfa34
Cristiano Coelho de Jesus Souza <cristiano.souza@concert.com.br>
2021-04-06 18:32:18 -0300

    Estrutura básica do projeto

---
+++ EventMonitor/Controllers/EventController.cs
@@ -0,0 +1,30 @@
+﻿using EventMonitor.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace EventMonitor.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult NewEvent([FromBody] Event data)
+        {
+            try
+            {
+
+
+                //TODO: Verificar e fazer um override ToString se necessário
+                return Ok($"Evento {data} recebido");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+
+            }
+        }
+    }
+}
---
+++ EventMonitor/Model/Event.cs
@@ -0,0 +1,9 @@
+﻿namespace EventMonitor.Model
+{
+    public class Event
+    {
+        public long Timestamp { get; set; }
+        public string Tag { get; set; }
+        public int Value { get; set; }
+    }
+}
