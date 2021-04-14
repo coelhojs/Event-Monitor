9d79ac4a444c3558ad6bf322d19598daec26bab3
Cristiano Coelho de Jesus Souza <cristiano.souza@concert.com.br>
2021-04-13 23:26:36 -0300

    ToString override do objeto de dado bruto.

--- server/EventMonitor/ViewObjects/RawEventVO.cs
+++ server/EventMonitor/ViewObjects/RawEventVO.cs
@@ -5,5 +5,10 @@
         public long Timestamp { get; set; }
         public string Tag { get; set; }
         public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"Tag: {Tag}, Timestamp: {Timestamp}, Value: {Value}";
+        }
     }
 }
